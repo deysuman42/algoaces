0772f2bc53e65ca074381b0e7c4b68439acef7cb
Suman Dey <sumandey@Sumans-MacBook-Pro.local>
2025-05-09 10:02:06 +0530

    Added data preparation notebook

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,13 @@ st.write(
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    openai_api_key == st.secrets["openai_api_key"],
+)
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
